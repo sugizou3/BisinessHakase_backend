18835f6cd2fb3fc12260f2788d36ca780bb2d7cf
sugiyamazou3 <sugiyama20010501@gmail.com>
2023-10-25 21:32:02 +0900

    postができない状態を解消中0

--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
+from django.views.static import serve
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),  #追加
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
