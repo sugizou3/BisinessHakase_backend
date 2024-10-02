52420ad56df253dcf9ff9c0506ceee85bebc8fe5
sugiyamazou3 <sugiyama20010501@gmail.com>
2024-10-02 20:01:46 +0900

    build.shの変更　migrateコマンドが読まれていない問題を解決

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py createsuperuser --username admin --email admin@test.com 
+python manage.py createsuperuser --username SUPERUSER_NAME --email qazwsx@edcrfv.com
